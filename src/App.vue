fced3d881e0595c1fda8f2a1a46d6a820c3f4e37
Jacob Patterson <jacob@jacobpa.com>
2019-10-13 13:37:00 -0400

    Fix linting warnings

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,8 @@ export default {
 #app {
   display: flex;
   flex-direction: column;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
   color: $black;
 
   .view-container {
