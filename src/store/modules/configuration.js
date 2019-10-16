0a413f2cbca4af9a28523423acefec515f10e67a
Jacob Patterson <jacob@jacobpa.com>
2019-10-16 12:51:08 -0400

    Add configuration state

---
+++ src/store/modules/configuration.js
@@ -0,0 +1,22 @@
+const state = {
+  minLength: 0,
+  minSpecialChar: 2,
+  minUpperChar: 2,
+};
+
+const mutations = {
+  setMinLength(state, { length }) {
+    state.minLength = length;
+  },
+  setMinSpecialChar(state, { count }) {
+    state.minSpecialChar = count;
+  },
+  setMinUpperChar(state, { count }) {
+    state.minUpperChar = count;
+  },
+};
+
+export default {
+  state,
+  mutations,
+};
