479bf7e25b338ef045e5db1d645751bd06f07f00
Jacob Patterson <jacob@jacobpa.com>
2019-11-12 15:17:41 -0500

    Export Vuex store directly instead of referencing separate objects

--- src/store/modules/configuration.js
+++ src/store/modules/configuration.js
@@ -1,34 +1,30 @@
-const state = {
-  password: '',
-  minLength: 8,
-  minSpecialChar: 2,
-  minUpperChar: 2,
-  minLowerChar: 2,
-};
-
-const mutations = {
-  setMinLength(state, length) {
-    if (!length || length < 0) return;
-    state.minLength = length;
-  },
-  setMinSpecialChar(state, count) {
-    if (!count || count < 0) return;
-    state.minSpecialChar = count;
-  },
-  setMinUpperChar(state, count) {
-    if (!count || count < 0) return;
-    state.minUpperChar = count;
-  },
-  setMinLowerChar(state, count) {
-    if (!count || count < 0) return;
-    state.minLowerChar = count;
-  },
-  setPassword(state, pass) {
-    state.password = pass;
-  },
-};
-
 export default {
-  state,
-  mutations,
+  state: {
+    password: '',
+    minLength: 8,
+    minSpecialChar: 2,
+    minUpperChar: 2,
+    minLowerChar: 2,
+  },
+  mutations: {
+    setMinLength(state, length) {
+      if (!length || length < 0) return;
+      state.minLength = length;
+    },
+    setMinSpecialChar(state, count) {
+      if (!count || count < 0) return;
+      state.minSpecialChar = count;
+    },
+    setMinUpperChar(state, count) {
+      if (!count || count < 0) return;
+      state.minUpperChar = count;
+    },
+    setMinLowerChar(state, count) {
+      if (!count || count < 0) return;
+      state.minLowerChar = count;
+    },
+    setPassword(state, pass) {
+      state.password = pass;
+    },
+  },
 };
