c0f9b450db1cea9c7bb15da00c591ecd4f72e945
Jacob Patterson <jacob@jacobpa.com>
2019-11-18 16:54:49 -0500

    Jacob doesn't know how to check value types in js

--- src/store/modules/configuration.js
+++ src/store/modules/configuration.js
@@ -8,19 +8,19 @@ export default {
   },
   mutations: {
     setMinLength(state, length) {
-      if (!length || length < 0) return;
+      if (typeof length !== 'number' || length < 0) return;
       state.minLength = length;
     },
     setMinSpecialChar(state, count) {
-      if (!count || count < 0) return;
+      if (typeof count !== 'number' || count < 0) return;
       state.minSpecialChar = count;
     },
     setMinUpperChar(state, count) {
-      if (!count || count < 0) return;
+      if (typeof count !== 'number' || count < 0) return;
       state.minUpperChar = count;
     },
     setMinLowerChar(state, count) {
-      if (!count || count < 0) return;
+      if (typeof count !== 'number' || count < 0) return;
       state.minLowerChar = count;
     },
     setPassword(state, pass) {
